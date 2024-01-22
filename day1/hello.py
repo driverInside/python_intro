be0c0ceea7d2d00fd8411dc1c473839e7ef2715e
Hiram E. Perez <hiram.eps@gmail.com>
2024-01-22 10:00:19 -0600

    first commit

---
+++ day1/hello.py
@@ -0,0 +1,5 @@
+print("Hello world!!")
+print("Hola mundo!!")
+print( "My name is Joshua" )
+print( "I am in grade 9" )
+print( "My favourite subject is Math" )
---
+++ day1/math.py
@@ -0,0 +1,30 @@
+'''
+math.py
+
+Basic math examples
+
++ - * / % > < >=
+                   2   <- cociente
+                 --------
+divisor ->   2  |  5    <- dividendo
+                   1    <- residuo
+
+= 
+'''
+
+# Print welcome message
+print("Welcome to python")
+# print('Lorem ipsum')
+
+print(3 + 9) # 12
+
+print("The sum of 3 + 9 is equal to:", 3 + 9)
+# print(5 + 9 / 4)
+# print(3 / 2)
+# print(3 % 2)
+print(22 % 8)
+
+print("22 is even if 22 % 2 is equal to 0", 22 % 2)
+
+print(8 < 3)
+
